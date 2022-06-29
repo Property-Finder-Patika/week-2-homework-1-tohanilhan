82f8fc825b2ed9ec006b9157a4dd0dde9749a29d
tohani <tohani@test.com>
2022-06-30 02:36:28 +0300

    variable names changed

--- coding-exercises/conversion.go
+++ coding-exercises/conversion.go
@@ -10,49 +10,53 @@ func main() {
 	// create conversion examples for the following basic types:
 	// int, float64, string, bool
 	// create a variable for conversion
-	var num int
+	var intNum int
 	// get the value from the user
+	fmt.Println("For int variables: ")
 	fmt.Println("Enter the number: ")
-	fmt.Scanln(&num)
+	fmt.Scanln(&intNum)
 	// convert the value to float64
-	fmt.Println("The float64 value of the number is: ", float64(num))
+	fmt.Println("The float64 value of the intNumber is: ", float64(intNum))
 	// convert the value to string
-	fmt.Println("The string value of the number is: ", strconv.Itoa(num))
+	fmt.Println("The string value of the intNumber is: ", strconv.Itoa(intNum))
 
 	// create a variable for conversion
-	var num2 float64
+	var floatintNum float64
 	// get the value from the user
-	fmt.Println("Enter the number: ")
-	fmt.Scanln(&num2)
+	fmt.Println("For float variables: ")
+	fmt.Println("Enter the floatNumber: ")
+	fmt.Scanln(&floatintNum)
 	// convert the value to int
-	fmt.Println("The int value of the number is: ", int(num2))
+	fmt.Println("The int value of the floatNumber is: ", int(floatintNum))
 	// convert the value to string
-	fmt.Println("The string value of the number is: ", strconv.FormatFloat(num2, 'f', -1, 64))
+	fmt.Println("The string value of the floatNumber is: ", strconv.FormatFloat(floatintNum, 'f', -1, 64))
 
 	// create a variable for conversion
-	var num3 string
+	var stringintNum string
 	// get the value from the user
-	fmt.Println("Enter the number: ")
-	fmt.Scanln(&num3)
+	fmt.Println("For string variables: ")
+	fmt.Println("Enter the stringNumber: ")
+	fmt.Scanln(&stringintNum)
 	// convert the value to int
-	num4, err := strconv.Atoi(num3)
+	intNum4, err := strconv.Atoi(stringintNum)
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
-	fmt.Println("The int value of the number is: ", num4)
+	fmt.Println("The int value of the stringNumber is: ", intNum4)
 	// convert the value to float64
-	num5, err := strconv.ParseFloat(num3, 64)
+	intNum5, err := strconv.ParseFloat(stringintNum, 64)
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
-	fmt.Println("The float64 value of the number is: ", num5)
+	fmt.Println("The float64 value of the stringNumber is: ", intNum5)
 
 	// create a variable for conversion
-	var num6 bool
+	var boolintNum bool
 	// get the value from the user
-	fmt.Println("Enter the number: ")
-	fmt.Scanln(&num6)
+	fmt.Println("For bool variables: ")
+	fmt.Println("Enter the boolNumber: ")
+	fmt.Scanln(&boolintNum)
 	// convert the value to string
-	fmt.Println("The string value of the number is: ", strconv.FormatBool(num6))
+	fmt.Println("The string value of the boolNumber is: ", strconv.FormatBool(boolintNum))
 
 }
