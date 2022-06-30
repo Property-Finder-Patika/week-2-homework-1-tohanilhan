781e94767a0e59e490ac1043db3b8d57240f2384
tohani <tohani@test.com>
2022-06-30 03:29:45 +0300

    First example added

---
+++ learngo/01-write-your-first-program/exercise/solution.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Tohan Atahan İlhan")
+	fmt.Println("Sertaç Yazıcı")
+}
+
+// You can basically run this program with go run main.go
+
+// Or you can build this program using go build
+// and run it by using ./solution
