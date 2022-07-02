0da77ac7a97795b74e9ff0ce51fc1e2c0c09eadf
tohani <tohani@test.com>
2022-07-02 23:52:09 +0300

    Example 4 added

---
+++ learngo/04-write-your-first-library-package/exercise/solution/golang/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt" // You should replace this with your username
+
+	"week-2-homework-1-tohanilhan/learngo/04-write-your-first-library-package/exercise/solution/golang"
+)
+
+func main() {
+	fmt.Println(golang.Version())
+}
